3aaacb6bb3fb7ff5f7d68baa5f508a102d0fb7b8
yhunlu <yhunlu87@gmail.com>
2022-02-25 03:14:24 +0300

    configured hardhat-config.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,21 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+const fs = require('fs');
+const privateKey = fs.readFileSync('.secret').toString();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: 'https://polygon-mumbai.g.alchemy.com/v2/Y22AtIifTIpHDxWiYaiCZUSbcllJEo5E',
+      accounts: [privateKey],
+    },
+    mainnet: {
+      url: 'https://polygon-mainnet.g.alchemy.com/v2/pUweezjH2rs8MAmOwW6m8g_tDM1oX34Z',
+      accounts: [privateKey],
+    },
+  },
+  solidity: '0.8.4',
 };
